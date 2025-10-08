46b28e2a595027891443b410245e841115bd2f37
develper21 <githubnarvin@gmail.com>
2025-10-08 22:49:03 +0530

    set all input fied color on black

--- reimburseui/src/app/workflows/page.tsx
+++ reimburseui/src/app/workflows/page.tsx
@@ -428,7 +428,7 @@ function WorkflowModal({
                 type="text"
                 value={formData.name}
                 onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-black"
                 required
               />
             </div>
@@ -486,7 +486,7 @@ function WorkflowModal({
                       e.target.value = ''
                     }
                   }}
-                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-black"
                 >
                   <option value="">Add an approver...</option>
                   {availableApprovers
